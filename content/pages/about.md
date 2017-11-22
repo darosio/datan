39e7be9ad3693b9deefb228c14ab4dfb21149513
daniele arosio <daniele.arosio@cnr.it>
2017-11-22 11:43:11 +0100

    ipynb ripristinati

---
+++ content/pages/about.md
@@ -0,0 +1,4 @@
+Title: About this site
+
+I'd like to use this space for keeping organized my [ipython
+notebook]-based data simulations. 
---
+++ pelicanconf.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'daniele arosio'
+SITENAME = 'datan'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# # Blogroll
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+         # ('Python.org', 'http://python.org/'),
+         # ('Jinja2', 'http://jinja.pocoo.org/'),
+         # ('You can modify those links in your config file', '#'),)
+
+# # Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+          # ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+STATIC_PATHS = ['images', 'figures', 'downloads']
+
+THEME = '../pelican-octopress-theme/'
+
+PLUGIN_PATHS = ['../pelican-plugins', 'plugins']
+PLUGINS = ['org_pandoc_reader', 'summary',
+           'liquid_tags.include_code',
+           'liquid_tags.literal',
+           'ipynb.markup',
+           'render_math']
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
